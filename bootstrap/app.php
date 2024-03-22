807492d0d8bae701848c9f7e0aebead0c685ca84
Carlos <carloscrespo@gmail.com>
2024-03-22 13:28:36 +0100

    [NEW] Task-5-Handling exception
    Problem
    * There is no error handling and it is recommended for PRODUCTION above all
    
    Solution
    * Added main exception user friendly messages

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Http\Middleware\API\V1\SetLocaleMiddleware;
+use Illuminate\Database\Eloquent\RelationNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Validation\UnauthorizedException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Exception\RouteNotFoundException;
 
@@ -30,11 +34,82 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
 
+        //NOTE:Use the following exceptions only for production or your capacity to find BUGs will be diminished
 
+        /**
+         * Unauthorized exception
+         */
+        $exceptions->render(function (UnauthorizedException $e, Request $request) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Unauthorized',
+            ], 401);
+        });
+
+        /**
+         * Route not found
+         */
         $exceptions->render(function (NotFoundHttpException $e, Request $request) {
             return response()->json([
                 'success' => false,
                 'message' => 'Route not found',
+            ], 404);
+        });
+
+        /**
+         * General error in DB query
+         */
+        $exceptions->render(function (QueryException $e, Request $request) {
+            if ($e->getCode() == 23000) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Query error 23000',
+                ], 409);
+            }
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Query error',
+            ], 404);
+        });
+
+        /**
+         * Error with method
+         */
+        $exceptions->render(function (MethodNotAllowedHttpException $e, Request $request) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Operation not found',
+            ], 404);
+        });
+
+        /**
+         * For type hint errors
+         */
+        $exceptions->render(function (TypeError $e, Request $request) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Check your request',
+            ], 404);
+        });
+
+        /**
+         * For relationship errors
+         */
+        $exceptions->render(function (RelationNotFoundException $e, Request $request) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Bad relationship provided or not found',
+            ], 404);
+        });
+
+        /**
+         * Other errors
+         */
+        $exceptions->render(function (\Exception $e, Request $request) {
+            return response()->json([
+                'success' => false,
+                'message' => 'An error occurred',
             ], 404);
         });
 
